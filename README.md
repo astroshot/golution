94319bab426bfdfe89bac6d4a00be955cf146772
liuzhao <liuzhaobuaa@163.com>
2018-02-13 22:36:40 +0800

    add lissajous and surface

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learning_go
+
+Begin learning golang with basic algorithms
+
---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "sync"
+
+    "learning_go/src/calc"
+)
+
+var mu sync.Mutex
+var count int
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    count++
+    mu.Unlock()
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    fmt.Fprintf(w, "Count %d\n", count)
+    mu.Unlock()
+}
+
+func plotSinc(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    calc.Draw()
+    mu.Unlock()
+}
+
+func plotLissajous(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    calc.Lissajous(w)
+    mu.Unlock()
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/count", counter)
+    http.HandleFunc("/lissajous", plotLissajous)
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
