a34047f5ce94067f5d4b418c95ed4c031e873d0e
liuzhao01 <liuzhao01@zhihu.com>
2018-02-14 11:14:07 +0800

    finish lissajous and sinc

--- src/calc/lissajous.go
+++ src/calc/lissajous.go
@@ -1,45 +1,43 @@
-package lissajous
+package calc
 
 import (
-    "image"
-    "image/color"
-    "image/gif"
-    "io"
-    "math"
-    "math/rand"
-    "os"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
 )
 
 var palette = []color.Color{color.White, color.Black}
 
 const (
-    whiteIndex = 0
-    blackIndex = 1
+	whiteIndex = 0
+	blackIndex = 1
 )
 
 func Lissajous(out io.Writer) {
-    const (
-        cycles = 5  // 完整的 x 振荡器变化的个数
-        res = 0.001  // 角度分辨率
-        size = 100  // 图像画布范围 [-size, size]
-        nframes = 64 // 动画帧数
-        delay = 8  // 以 10ms 为单位的帧间延迟
-    )
-    freq := rand.Float64() * 3.0  // y 振荡器的相对频率
-    anim := gif.GIF{LoopCount: nframes}
-    phase := 0.0
-    for i := 0; i < nframes; i++ {
-        rect := image.Rect(0, 0, 2 * size + 1, 2 * size + 1)
-        img := image.NewPaletted(rect, palette)
-        for t := 0.0; t < cycles * 2 * math.Pi; t += res {
-            x := math.Sin(t)
-            y := math.Sin(t * freq + phase)
-            img.SetColorIndex(size + int(x*size+0.5), size+int(y*size+0.5), blackIndex)
-        }
-        phase += 0.1
-        anim.Delay = append(anim.Delay, delay)
-        anim.Image = append(anim.Image, img)
-    }
-    gif.EncodeAll(out, &anim)
+	const (
+		cycles  = 5     // 完整的 x 振荡器变化的个数
+		res     = 0.001 // 角度分辨率
+		size    = 100   // 图像画布范围 [-size, size]
+		nframes = 64    // 动画帧数
+		delay   = 8     // 以 10ms 为单位的帧间延迟
+	)
+	freq := rand.Float64() * 3.0 // y 振荡器的相对频率
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
 }
-
