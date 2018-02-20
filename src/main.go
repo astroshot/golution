deeb4b48adbb05edd91c16ab2468ca062856a787
liuzhao01 <liuzhao01@zhihu.com>
2018-02-20 17:07:26 +0800

    move handlers to one package

--- src/main.go
+++ src/main.go
@@ -4,44 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 
-	"learning_go/src/calc"
+	web "learning_go/src/web"
 )
 
-var mu sync.Mutex
-var count int
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
-}
-
-func plotSinc(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	r.Header.Set("Content-Type", "image/svg+xml")
-	calc.DrawSinc(w)
-	mu.Unlock()
-}
-
-func plotLissajous(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	calc.Lissajous(w)
-	mu.Unlock()
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
-	http.HandleFunc("/lissajous", plotLissajous)
-	http.HandleFunc("/sinc", plotSinc)
+	fmt.Println("Initializing...")
+	http.HandleFunc("/", web.DefaultHandler)
+	http.HandleFunc("/count", web.CounterHandler)
+	http.HandleFunc("/lissajous", web.PlotLissajousHandler)
+	http.HandleFunc("/sinc", web.PlotSincHandler)
+
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
---
+++ src/web/handlers.go
@@ -0,0 +1,38 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"learning_go/src/calc"
+)
+
+var mu sync.Mutex
+var count int
+
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func CounterHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func PlotSincHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	r.Header.Set("Content-Type", "image/svg+xml")
+	calc.DrawSinc(w)
+	mu.Unlock()
+}
+
+func PlotLissajousHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	calc.Lissajous(w)
+	mu.Unlock()
+}
